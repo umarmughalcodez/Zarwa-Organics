5176298515a55fc82ce85452ad0aa972a796dd79
Umar <u4umarmughal@gmail.com>
2025-11-11 20:47:12 +0500

    ready for debugging

--- app/api/order/[orderId]/route.ts
+++ app/api/order/[orderId]/route.ts
@@ -1,12 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { orderId: string } }
-) {
+export async function GET({
+  params,
+}: {
+  params: Promise<{ orderId: string }>;
+}) {
   try {
-    const { orderId } = params;
+    const { orderId } = await params;
 
     const order = await prisma.order.findUnique({
       where: { id: orderId },
